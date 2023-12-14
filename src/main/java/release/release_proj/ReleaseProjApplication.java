a4d2e461c39a68dbab3ad16257ac0114dcd61e51
lee0594 <dlthdus0220@naver.com>
2023-12-14 13:25:21 +0900

    Fix: orderDate 자동되도록 코드 수정

--- src/main/java/release/release_proj/ReleaseProjApplication.java
+++ src/main/java/release/release_proj/ReleaseProjApplication.java
@@ -2,7 +2,9 @@ package release.release_proj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //orderDate 자동생성 위함
 @SpringBootApplication
 public class ReleaseProjApplication {
 
--- src/main/java/release/release_proj/domain/Order.java
+++ src/main/java/release/release_proj/domain/Order.java
@@ -1,6 +1,8 @@
 package release.release_proj.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,14 +13,16 @@ import java.time.LocalDateTime;
 @Getter
 @Setter //dto 적용 시 setter 생략하기
 @Entity
+@EntityListeners(AuditingEntityListener.class) //orderDate 자동생성 위함
 @Table(name = "orders") //order: 예약어 -> table 이름으로 사용 불가
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
     private Long orderId;
-    private String memberId;  //ManyTonOne Mapping 등 이용할건지 생각해보기(그러면 외래키 예외처리할 필요x, getAmount 등의 함수 만들 필요 x)
+    private String memberId;  //ManyTonOne Mapping 등 이용할건지 생각해보기(그러면 db 자동생성해서 외래키 예외처리할 필요x, itemService.getAmount, getPrice 함수 만들 필요 x)
     private Long itemId;
+    @CreatedDate
     private LocalDateTime orderDate;
     private int price; //총 가격
     private int count; //구매 개수
