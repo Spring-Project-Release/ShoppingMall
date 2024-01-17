e6d827ee47ab3328ce980e9af97137f1002c2eaa
Lee-Junmo <108059303+ezurno@users.noreply.github.com>
2024-01-17 16:56:35 +0900

    Fix: with-credentials

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { ILoginFormData } from "./interface";
 
+const api = axios.create({
+  withCredentials: true,
+});
+
 export function postLoginData(data: ILoginFormData) {
   let url = `${process.env.REACT_APP_BASE_URL}/user/login`;
-  return axios.post(url, data);
+  return api.post(url, data);
 }
 
 export function getLogoutData(memberId: string) {
   let url = `${process.env.REACT_APP_BASE_URL}/user/logout`;
-  return axios.get(url, {
+  return api.get(url, {
     params: {
       memberId,
     },
